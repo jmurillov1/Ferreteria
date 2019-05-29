499d3135b9cf584cae277dd52c45dcea076fc1b2
nikoap77 <nikoap77@gmail.com>
2019-05-29 16:52:13 -0500

    Ferreteria

--- admin/vista/crear_producto.php
+++ admin/vista/crear_producto.php
@@ -4,23 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <title>Sistema de Gestion de Ferretería</title>
-    <link href="../../public/vista/css/style.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="../controladores/js/funciones.js"> </script>
-    <!--<script type="text/javascript" src="../controladores/js/validaciones.js"> </script>-->
+    <link href="../../public/vista/css/estilo.css" rel="stylesheet" />
+    <a href="../../public/vista/index.html"><img id="cen" src="../../public/imagenes/logo.png"></a>
 </head>
 
 <body>
-    <header class="cab">
-        <h1>Insertar Nuevo Producto</h1>
-    </header>
-    <form id="formulario01" method="POST" enctype="multipart/form-data" action="../controladores/crear_producto.php">
+    <h1>Insertar Nuevo Producto</h1>
+
+    <form id="form" method="POST" enctype="multipart/form-data" action="../controladores/crear_producto.php">
         <div class=" parte1">
             <label for="nombre">Nombre(*)</label>
-            <input type="text" id="nombre" name="nombre" value="" placeholder="Ingrese el nombre del producto ..." />
+            <input class="in" type="text" id="nombre" name="nombre" value="" placeholder="Ingrese el nombre del producto ..." />
             <span id="mensajeNombre" class="error"> </span>
             <br>
             <label for="desc">Descripción(*)</label>
-            <input type="text" id="desc" name="desc" value="" placeholder="Ingrese su descripción ..." />
+            <input class="in" type="text" id="desc" name="desc" value="" placeholder="Ingrese su descripción ..." />
             <span id="mensajeDescripcion" class="error"></span>
             <br>
             <label for="desc">Categoria(*)</label>
@@ -50,10 +48,53 @@
         </div>
         <div class=" parte2">
             <label for="imagen">Seleccione imagen a cargar</label>
-            <input id="uploadImage1" type="file" name="image" onchange="previewImage(1)" accept="image/*" />
+            <input class="in" id="uploadImage1" type="file" name="image" onchange="previewImage(1)" accept="image/*" />
             <img id="uploadPreview1" width="150" height="150" src="images/usu.PNG" />
         </div>
     </form>
+
+    <br>
+
+    <footer>
+        <section id="pa">
+            <h2>
+                INFORMACIÓN DE CONTACTO
+            </h2>
+            <h3>
+                DIRECCION
+            </h3>
+            <h4>
+                Av. Gil Ramirez Davalos y Eliat Liut
+            </h4>
+            <h3>
+                TELEFONO
+            </h3>
+            <h3>
+                0981241115 - 0989224223
+            </h3>
+            <h3>
+                EMAIL
+            </h3>
+            <h4>
+                servicio@tupernoferreteria.com
+            </h4>
+
+        </section>
+        <section id="fot">
+            <h2>REDES SOCIALES</h1>
+                <div>
+                    <ul>
+                        <li><a href="https://www.facebook.com/niko.anazco.1" target="_blank"><img src="../../public/imagenes/fac.png" width=80px heidth=180px></a></li>
+                        <li><a href="https://mail.google.com/mail/" target="_blank"><img src="../../public/imagenes/cor.png" width=80px heidth=120px></a></li>
+                        <li><a href="https://twitter.com/Nik_Augusto?lang=es" target="_blank"><img src="../../public/imagenes/twi.png" width=80px heidth=100px></a></li>
+                        <li><a href="https://www.instagram.com/nikoap77/" target="_blank"><img src="../../public/imagenes/ins.png" width=80px heidth=100px></a></li>
+                    </ul>
+                </div>
+        </section>
+        <section id="fot1">
+            <h2>© Copyright 2019 Powered by MurilloJ, A&ntilde;azcoN, BenavidezA </h1>
+        </section>
+    </footer>
 </body>
 
 </html>
--- admin/vista/crear_suc_producto.php
+++ admin/vista/crear_suc_producto.php
@@ -4,16 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Sistema de Gestion de Ferretería</title>
-    <link href="../../public/vista/css/style.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="../controladores/js/funciones.js"> </script>
-    <!--<script type="text/javascript" src="../controladores/js/validaciones.js"> </script>-->
+    <link href="../../public/vista/css/estilo.css" rel="stylesheet" />
+    <a href="../../public/vista/index.html"><img id="cen" src="../../public/imagenes/logo.png"></a>
 </head>
 
 <body>
     <header class="cab">
-        <h1>Insertar Nuevo Producto</h1>
+        <h1>Insertar Nueva Sucursal Producto</h1>
     </header>
-    <form id="formulario01" method="POST" action="../controladores/crear_suc_producto.php">
+    <form id="form" method="POST" action="../controladores/crear_suc_producto.php">
         <div class=" parte1">
             <label for="producto">Producto(*)</label>
             <select id="producto" name="producto">
@@ -34,7 +33,7 @@
             <span id="mensajeProducto" class="error"> </span>
             <br>
             <label for="stock">Stock(*)</label>
-            <input type="text" id="stock" name="stock" value="" placeholder="Ingrese su descripción ..." />
+            <input class="in" type="text" id="stock" name="stock" value="" placeholder="Ingrese su descripción ..." />
             <span id="mensajeStock" class="error"></span>
             <br>
             <label for="sucursal">Sucursal(*)</label>
@@ -54,10 +53,56 @@
             </select>
             <span id="mensajeSucursal" class="error"></span>
             <br>
-            <input type="submit" id="crear" name="crear" value="Aceptar" />
-            <input type="reset" id="cancelar" name="cancelar" value="Cancelar" />
+            <input class="in" type="submit" id="crear" name="crear" value="Aceptar" />
+            <input class="in" type="reset" id="cancelar" name="cancelar" value="Cancelar" />
         </div>
     </form>
+
+    <br>
+
+    <footer>
+        <section id="pa">
+            <h2>
+                INFORMACIÓN DE CONTACTO
+            </h2>
+            <h3>
+                DIRECCION
+            </h3>
+            <h4>
+                Av. Gil Ramirez Davalos y Eliat Liut
+            </h4>
+            <h3>
+                TELEFONO
+            </h3>
+            <h3>
+                0981241115 - 0989224223
+            </h3>
+            <h3>
+                EMAIL
+            </h3>
+            <h4>
+                servicio@tupernoferreteria.com
+            </h4>
+
+        </section>
+
+        <section id="fot">
+            <h2>REDES SOCIALES</h1>
+                <div>
+                <ul>
+                        <li><a href="https://www.facebook.com/niko.anazco.1" target="_blank"><img src="../../public/imagenes/fac.png" width=80px heidth=180px></a></li>
+                        <li><a href="https://mail.google.com/mail/" target="_blank"><img src="../../public/imagenes/cor.png" width=80px heidth=120px></a></li>
+                        <li><a href="https://twitter.com/Nik_Augusto?lang=es" target="_blank"><img src="../../public/imagenes/twi.png" width=80px heidth=100px></a></li>
+                        <li><a href="https://www.instagram.com/nikoap77/" target="_blank"><img src="../../public/imagenes/ins.png" width=80px heidth=100px></a></li>
+                    </ul>
+                </div>
+        </section>
+
+        <section id="fot1">
+            <h2>© Copyright 2019 Powered by MurilloJ, A&ntilde;azcoN, BenavidezA </h1>
+        </section>
+    </footer>    
+
 </body>
 
 </html>
